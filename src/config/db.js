9aece50ea43827e5c7ba3c2524f0b19154e56189
ezeqduarte <ezeqduarte01@gmail.com>
2025-11-16 20:30:22 -0300

    FEAT: Updated db config

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB conectado:", conn.connection.name);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB conectado");
   } catch (error) {
-    console.error("Error al conectar a MongoDB:", error.message);
+    console.error("Error al conectar MongoDB:", error.message);
     process.exit(1);
   }
 }
