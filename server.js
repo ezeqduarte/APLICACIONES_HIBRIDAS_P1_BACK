8f930f66a712894b503e79286552d3b406aa697b
ezeqduarte <ezeqduarte01@gmail.com>
2025-12-06 16:59:18 -0300

    feat: test deploy vercel

--- server.js
+++ server.js
@@ -33,6 +33,9 @@ app.use("/api/user", userRoutes);
 app.use("/api/comments", commentRoutes);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () =>
     console.log(`Servidor corriendo en puerto ${PORT}`)
 );
+
+export default app;
